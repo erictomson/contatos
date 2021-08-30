62b1e6f918e2415b115ef6da51e23021b7015c7e
erictomson <erictomson@gmail.com>
2021-08-30 12:08:43 -0300

    Criado o model Contato e listando os Contatos na Lista de Contaos (sem os dados)

--- src/app/components/lista-de-contatos/lista-de-contatos.component.ts
+++ src/app/components/lista-de-contatos/lista-de-contatos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Contato } from 'src/app/model/Contato';
+import { Contato } from 'src/app/models/Contato';
 
 @Component({
   selector: 'app-lista-de-contatos',
@@ -7,6 +7,58 @@ import { Contato } from 'src/app/model/Contato';
   styleUrls: ['./lista-de-contatos.component.css']
 })
 export class ListaDeContatosComponent implements OnInit {
+
+  contatos: Contato[] = [
+    {
+      nome: "<ariana Ribeiro 1",
+      email: "mariana@teste.com",
+      telefones: ['1111-1111']
+    },
+    {
+      nome: "<ariana Ribeiro 2",
+      email: "mariana@teste.com",
+      telefones: ['1111-1111']
+    },
+    {
+      nome: "<ariana Ribeiro 3",
+      email: "mariana@teste.com",
+      telefones: ['1111-1111']
+    },
+    {
+      nome: "<ariana Ribeiro 4",
+      email: "mariana@teste.com",
+      telefones: ['1111-1111']
+    },
+    {
+      nome: "<ariana Ribeiro 5",
+      email: "mariana@teste.com",
+      telefones: ['1111-1111']
+    },
+    {
+      nome: "<ariana Ribeiro 6",
+      email: "mariana@teste.com",
+      telefones: ['1111-1111']
+    },
+    {
+      nome: "<ariana Ribeiro 7",
+      email: "mariana@teste.com",
+      telefones: ['1111-1111']
+    },
+    {
+      nome: "<ariana Ribeiro 1",
+      email: "mariana@teste.com",
+      telefones: ['1111-1111']
+    },
+    {
+      nome: "<ariana Ribeiro 8",
+      email: "mariana@teste.com",
+      telefones: ['1111-1111']
+    },
+    {
+      nome: "<ariana Ribeiro 9",
+      email: "mariana@teste.com",
+      telefones: ['1111-1111']
+    }];
 
   constructor() { }
 
